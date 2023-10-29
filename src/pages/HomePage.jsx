349c445faef96233823906c23f28e7ce5eabf68f
Chase <chasegarrett@tutanota.com>
2023-10-29 17:21:24 -0500

    add homepage styling

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,42 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
+
 export default function HomePage() {
+  const styles = {
+    box: {
+      backgroundColor: "#22162B",
+      display: "flex",
+      flexDirection: "column",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      width: "100vw",
+      overflowX: "hidden",
+    },
+    typography: {
+      color: "#ffffff",
+      textAlign: "center",
+      textShadow: "1px 1px 10px #668a64",
+      fontFamily: "Oswald, sans-serif",
+    },
+  };
+
   return (
-    <div>
-      <p>Welcome Home!</p>
-    </div>
+    <Box disableGutters sx={styles.box}>
+      <Typography variant="h2" component="h2" sx={styles.typography}>
+        Chase Garrett
+      </Typography>
+      <Typography
+        variant="h1"
+        component="h1"
+        gutterBottom="true"
+        sx={styles.typography}
+      >
+        Full-Stack Solutions
+      </Typography>
+      <Typography variant="h6" component="h6" sx={styles.typography}>
+        Bespoke, elegant tech solutions tailored for your needs.
+      </Typography>
+    </Box>
   );
 }
